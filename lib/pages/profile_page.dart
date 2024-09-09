0e7c885edb8d81c520e10b43171bb66853b3e79b
Hyoin Lee <winroof1988@gmail.com>
2024-09-09 14:22:48 +0800

    refactor(change password): Add current password field, fix(profile): Fixing changes made in edit profile apply to profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -7,10 +7,13 @@ class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
