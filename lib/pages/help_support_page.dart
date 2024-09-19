bd6c286fa06e5e070ea562c8b942005db669a00c
NorthBridge Trainee <nbt@tdm.local>
2024-09-19 10:59:40 +0800

    Add(help_support_page) link to TAFE helpdesk

--- lib/pages/help_support_page.dart
+++ lib/pages/help_support_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpSupportPage extends StatelessWidget {
   const HelpSupportPage({super.key});
+
+  void _launchURL(BuildContext context) async {
+    final Uri url = Uri.parse('https://help.screencraft.net.au/help/2680392001');
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl (url, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +22,22 @@ class HelpSupportPage extends StatelessWidget {
         title: const Text('Help & Support'),
         backgroundColor: Colors.deepPurple,
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.all(18.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Find help and support here',
-              style: TextStyle(fontSize: 18),
-            ),
+            GestureDetector(
+              onTap: () => _launchURL(context),
+              child: const Text(
+                'Find help and support here',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                )
+                ),
+            )
           ],
         ),
       ),
